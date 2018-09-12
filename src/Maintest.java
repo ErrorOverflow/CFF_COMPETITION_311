fa0509c7ea7109b76d9fa7a458ef966bd8dec772
wml <32595386+wmlbuaa@users.noreply.github.com>
2018-09-12 22:58:00 +0800

    gender-type input

--- src/Maintest.java
+++ src/Maintest.java
@@ -3,6 +3,23 @@ import java.io.*;
 public class Maintest {
     public static void main(String[] args) throws IOException {
         TypeTag[] typeTags = new TypeTag[15];
+        typeTags[0]=new TypeTag(89016252);
+        typeTags[1]=new TypeTag(89016253);
+        typeTags[2]=new TypeTag(89016259);
+        typeTags[3]=new TypeTag(89950166);
+        typeTags[4]=new TypeTag(89950167);
+        typeTags[5]=new TypeTag(89950168);
+        typeTags[6]=new TypeTag(90063345);
+        typeTags[7]=new TypeTag(90109916);
+        typeTags[8]=new TypeTag(90155946);
+        typeTags[9]=new TypeTag(99104722);
+        typeTags[10]=new TypeTag(99999825);
+        typeTags[11]=new TypeTag(99999826);
+        typeTags[12]=new TypeTag(99999827);
+        typeTags[13]=new TypeTag(99999828);
+        typeTags[14]=new TypeTag(99999830);
+
+
         Calculator calculator = new Calculator(typeTags);
         UserInfo[] userInfo = new UserInfo[100000];
 
@@ -13,8 +30,8 @@ public class Maintest {
         while ((line = bufferedReader.readLine()) != null && i++ < 99999) {
             userInfo[i] = new UserInfo(line, calculator);
         }
-        for (int j = 0; j < calculator.current_type.size(); j++) {
-            System.out.println(calculator.current_type.get(j));
+        for(int j=0;j<15;j++){
+            System.out.println(typeTags[j].getTag()+" "+typeTags[j].getGender()[0]+" "+typeTags[j].getGender()[1]);
         }
         //new Output(userInfo);
     }
--- src/TypeTag.java
+++ src/TypeTag.java
@@ -1,58 +1,24 @@
 public class TypeTag {
-    public int[] gender=new int[2];
+    public int tag;
+    public int[] gender = new int[2];
 
-    public void addGender(int i){
-        if(i==1){
+    public TypeTag(int i) {
+        this.tag = i;
+    }
+
+    public void addGender(int i) {
+        if (i == 1) {
             gender[0]++;
-        }else if(i==2){
+        } else if (i == 2) {
             gender[1]++;
         }
     }
+
+    public int[] getGender() {
+        return gender;
+    }
+
+    public int getTag() {
+        return tag;
+    }
 }
-
-class type_99999825 extends TypeTag{
-
-}
-class type_90063345 extends TypeTag{
-
-}
-class type_90109916 extends TypeTag{
-
-}
-class type_89950166 extends TypeTag{
-
-}
-class type_89950168 extends TypeTag{
-
-}
-class type_99104722 extends TypeTag{
-
-}
-class type_89950167 extends TypeTag{
-
-}
-class type_89016252 extends TypeTag{
-
-}
-class type_90155946 extends TypeTag{
-
-}
-class type_99999828 extends TypeTag{
-
-}
-class type_99999826 extends TypeTag{
-
-}
-class type_99999827 extends TypeTag{
-
-}
-class type_89016259 extends TypeTag{
-
-}
-class type_99999830 extends TypeTag{
-
-}
-class type_89016253 extends TypeTag{
-
-}
-
