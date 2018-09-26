a97b2d6d4f5d512d0e217128a3c356fb3ae3c519
wml <32595386+wmlbuaa@users.noreply.github.com>
2018-09-26 19:36:53 +0800

    0 80301 13688 0.8543659364393705

--- src/Maintest.java
+++ src/Maintest.java
@@ -3,9 +3,9 @@ import java.io.*;
 public class Maintest {
     //0.LocalCallerTime(ManyOverBill) --> 1.OnlineTime(FEE2) -->  2.MonthTraffic --> 3.ContractTime√ --> 4.LocalTrafficMonth(ServiceType)√
     // --> 5.NetService(FEE3) --> 6.LastMonthTrafiic(FEE1)√ --> 7.gender(Service_1_caller) --> 8.Service_2_caller
-    // --> 8._1Node() --> 9..PayNum(LastMonthTraffic)
-    public static final int[] SPLIT_SPACE = {1, 32, 437, 11, 1, 41, 52, 31, 49, 2000, 55};
-    public static final int[] SPLIT_NUM = {5, 10, 5, 4, 5, 5, 16, 2, 16, 4, 70};
+    // --> 8._1Node(local_traffic_month) --> 9..PayNum(LastMonthTraffic)
+    public static final int[] SPLIT_SPACE = {1, 32, 527, 11, 1, 41, 51, 1, 51, 20000, 55};
+    public static final int[] SPLIT_NUM = {5, 10, 3, 4, 5, 5, 4, 2, 10, 2, 70};
     public static final String readPath = "C:\\Users\\Buaa-Aladdin\\Downloads\\train_all.csv";
     public static final String testPath = "C:\\Users\\Buaa-Aladdin\\Downloads\\train_all.csv";
     public static final String writePath = "C:\\result.csv";
